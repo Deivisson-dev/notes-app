f6afd3be78c03c9bf577a1116285483513728559
Deivisson-dev <darsonsilva83@gmail.com>
2024-02-09 22:07:18 -0300

    Add search functionality and note creation/deletion

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,16 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard() {
+
+interface NewNoteCardProps {
+    onNoteCreated: (content: string) => void
+
+}
+
+
+
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
     const [content, setContent] = useState('')
 
@@ -23,7 +32,10 @@ export function NewNoteCard() {
     function handleSaveNote(event: FormEvent) {
         event.preventDefault()
 
-        console.log(content)
+        onNoteCreated(content)
+
+        setContent('')
+        setShouldShowOnboarding(true)
 
         toast.success('Nota criada com sucesso!')
     }
@@ -41,7 +53,7 @@ export function NewNoteCard() {
 
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-                <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+                <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'>
                     <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5'/>
                     </Dialog.Close>
@@ -58,9 +70,10 @@ export function NewNoteCard() {
                                 </p>
                             ) : (
                                 <textarea 
-                                autoFocus
-                                className='text-sm leading-6 bg-transparent outline-none resize-none text-slate-400 flex-1'
-                                onChange={handleContentChanged}
+                                    autoFocus
+                                    className='text-sm leading-6 bg-transparent outline-none resize-none text-slate-400 flex-1'
+                                    onChange={handleContentChanged}
+                                    value={content}
                                 />
                             )}
                         </div>
